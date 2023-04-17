8be846f8cde018f55982ebaa0b117bab60428bf3
Abdul Rizwan <abdulrizwan706@gmail.com>
2023-04-17 16:11:42 +0530

    this is done

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,13 @@
 public class Person {
-// write the code of Person class here
+    //Create a class Person with following requirements
+
+    // Should have a data-member age
+    // Should have a data-member name
+    // Should support a constructor with both age and name
+    int age;
+    String name;
+    public Person(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
 }
